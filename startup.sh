d2150689303e7d1216026be6879fef6c46c50787
Jason Hughes <jhughes@reachablegames.com>
2020-09-25 01:34:16 -0500

    I think this version might work.  Fixed the detection of Grav being installed.

--- startup.sh
+++ startup.sh
@@ -4,11 +4,11 @@ set -x
 set -e
 
 # copy the grav install folder to the html folder if missing.  Helps with new setups.
-if [ -e /var/www/html/index.php ]; then
+if [ -e /var/www/html/user ]; then
 	echo "[ INFO ] Grav already exists"
 else
 	echo "[ INFO ] Grav is missing.  Copying into web folder."
-	cp -Rp /var/www/grav-admin/* /var/www/html/
+	cp -R /var/www/grav-admin/* /var/www/html/
 fi
 
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
